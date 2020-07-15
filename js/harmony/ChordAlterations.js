4e9d72c1033650ff68dc899da014f69b3d3e3ad1
Jeroen Donkers <jeroen.donkers@live.nl>
2020-07-15 16:27:47 +0200

    Removed b11 (its just the third) and the #13 (its just the b7)

--- js/harmony/ChordAlterations.js
+++ js/harmony/ChordAlterations.js
@@ -18,12 +18,8 @@ const ChordAlteration = function(name, alter) {
 const ChordAlterations = {
     b9: ChordAlteration("b9", intervals => flatten(intervals, 14)),
     s9: ChordAlteration("#9", intervals => sharpen(intervals, 14)),
-
-    b11: ChordAlteration("b11", intervals => flatten(intervals, 17)),
     s11: ChordAlteration("#11", intervals => sharpen(intervals, 17)),
-
     b13: ChordAlteration("b13", intervals => flatten(intervals, 21)),
-    s13: ChordAlteration("#13", intervals => sharpen(intervals, 21)),
 
     [Symbol.iterator]: function* () { for (let key in this) if (this.hasOwnProperty(key) && key !== "random" && key !== "fromName") yield this[key]; },
     random: function(filter) {
