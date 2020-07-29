5f9c4671eaa3f84f4d6040253e9c83f0d107da9c
Jeroen Donkers <jeroen.donkers@live.nl>
2020-07-29 18:16:16 +0200

    Improved showing the input notes in the UiInputViewer.js

--- js/ui/UiInputViewer.js
+++ js/ui/UiInputViewer.js
@@ -1,16 +1,22 @@
 import Events from "../app/events/Events.js";
 import MidiNotes from "../midi/MidiNotes.js";
 
+const keyToString = key => key ? MidiNotes.keyToString(key) : "";
+
+const NoteOnStack = function(textElement) {
+    const stack = [];
+    const fireStackChanged = () => textElement.text(keyToString(stack[0]));
+    return {
+        push: key => { stack.unshift(key); fireStackChanged(); },
+        pop: key => { stack.splice(stack.indexOf(key), 1); fireStackChanged(); },
+    };
+};
+
 const UiInputViewer = function() {
-    let midiInputNote = $("#midiInputNote");
+    const noteOnStack = NoteOnStack($("#midiInputNote"));
 
-    Events.subscribe(Events.Midi.Devices.Input.noteOnReceived, function(e) {
-        midiInputNote.text(MidiNotes.keyToString(e.midiMessage.key));
-    });
-
-    Events.subscribe(Events.Midi.Devices.Input.noteOffReceived, function() {
-        midiInputNote.text("");
-    });
+    Events.subscribe(Events.Midi.Devices.Input.noteOnReceived, e => noteOnStack.push(e.midiMessage.key));
+    Events.subscribe(Events.Midi.Devices.Input.noteOffReceived, e => noteOnStack.pop(e.midiMessage.key));
 };
 
 export default UiInputViewer;
